1665a2f65952a618168925d3010392891225a167
Jarryd Semmens <JarrydSemmens@hotmail.com>
2018-01-27 01:01:23 +1100

    ~ Added: Alien class, with random name generation ~ Added: Score manager with basic uber rating support ~ Added: alien prefab ~ Added: score manager prefab

---
+++ GameJam2018/Assets/Scripts/Manager/Alien.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Randomized passenger with both initial, and ongoing requirements
+/// </summary>
+public class Alien : MonoBehaviour
+{
+    public string RacialName = "Alien";
+
+    private List<string> NamePrefix = new List<string>()
+    {
+        "Mee",
+        "Cronen",
+        "Gromfla",
+        "Hu",
+        "Gazor",
+        "Butter",
+    };
+
+    private List<string> NamePostfix = new List<string>()
+    {
+        "seek",
+        "berg",
+        "mite",
+        "man",
+        "pian",
+        "ions"
+    };
+
+    public void RegenerateRacialName()
+    {
+        RacialName = NamePrefix[(int)Random.Range(0, NamePrefix.Count)] + NamePostfix[(int)Random.Range(0, NamePostfix.Count)];        
+    }
+    
+
+    /// <summary>
+    /// The gas required to be happy and survive
+    /// </summary>
+    public Gas BreathableAtmosphere;
+        
+
+	// Use this for initialization
+	void Start ()
+    {
+        RegenerateRacialName();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GameJam2018/Assets/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    /// <summary>
+    /// The current score rating being left by the Alien Pax
+    /// </summary>
+    public float UberRating { get; protected set; }      
+
+    public void DamageUberRating (float in_Damage)
+    {
+        Debug.Assert(in_Damage > 0, "Damage must be great than 0");
+        Debug.Assert(in_Damage < 5, "Damage cannot exceed 5");
+
+        UberRating -= in_Damage;
+    }
+
+    public void ImproveUberRating(float in_Improvement)
+    {
+        Debug.Assert(in_Improvement > 0, "Improvement must be great than 0");
+        Debug.Assert(in_Improvement < 5, "Improvement cannot exceed 5");
+
+        UberRating += in_Improvement;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
