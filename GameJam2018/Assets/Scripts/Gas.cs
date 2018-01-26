825e84812a39ec7cc9962805a912c8a5ebca05b7
Joshua Reason <reason.joshua@gmail.com>
2018-01-26 23:07:24 +1100

    Add gas manager class + gas class

---
+++ GameJam2018/Assets/Scripts/Gas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GasType
+{
+    Null,
+    Oxygen,
+    Carbon_Dioxide,
+    Nitrogen
+}
+
+
+    /// <summary>
+    /// Struct for hoild ing gas types
+    /// </summary>
+    /// 
+[System.Serializable]
+public class Gas {
+
+    public GasType GasType;
+    public float Percentage;
+
+    public Gas(GasType type)
+    {
+        this.GasType = type;
+        Percentage = 0;
+    }
+}
---
+++ GameJam2018/Assets/Scripts/Manager/AtmosphereManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtmosphereManager : MonoBehaviour {
+
+    //Static Acess to this class set on awake
+    public static AtmosphereManager Instance;
+
+
+    public List<Gas> AllGases = new List<Gas>();
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+    public static Gas GetGas(GasType in_Type)
+    {
+        foreach (Gas gas in Instance.AllGases)
+        {
+            if (gas.GasType.Equals(in_Type))
+                return gas;
+        }
+        return new Gas(GasType.Null);
+    }
+
+    public static void SetGas(GasType in_Type, float in_Percentage)
+    {
+        foreach (Gas gas in Instance.AllGases)
+        {
+            if (gas.GasType.Equals(in_Type))
+                gas.Percentage = in_Percentage;           
+        }
+    }
+
+}
