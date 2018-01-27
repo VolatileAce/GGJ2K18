3d3882bec167ce32813338ea9a9b1f1d3ff1b75f
Jarryd Semmens <JarrydSemmens@hotmail.com>
2018-01-28 03:56:14 +1100

    ~ Added: Some splashes ~ Added: Main menu ~ Added: credits

---
+++ GameJam2018/Assets/Scripts/Manager/SpaceFlightManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceFlightManager : MonoBehaviour
+{
+    public static SpaceFlightManager Instance;
+
+    public ParticleSystem HyperspaceParticles;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GameJam2018/Assets/SplashScreenScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreenScript : MonoBehaviour
+{
+    public float TransitionDelay = 0.5f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //StartCoroutine("TimedTransitionToMainMenu");
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    private IEnumerator TimedTransitionToMainMenu()
+    {
+        yield return new WaitForSeconds(TransitionDelay);
+        Application.LoadLevel(1);
+    }
+
+    public void OnMainMenuClick()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void OnPlayClick()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void OnCreditsClick()
+    {
+        Application.LoadLevel(2);
+    }
+
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
