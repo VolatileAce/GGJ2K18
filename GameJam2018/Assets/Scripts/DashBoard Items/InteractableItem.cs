8262b96c3216e11cfe247287178dcc8479f8dd1b
Joshua Reason <reason.joshua@gmail.com>
2018-01-27 19:41:33 +1100

    Working buttons!

---
+++ GameJam2018/Assets/Scripts/DashBoard Items/InteractableItem.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class InteractableItem : MonoBehaviour {
+
+    public Material ButtonLightMat;
+    public Color BaseColor;
+    public Color ClickedColor;
+
+    protected Material LightMatInstance;
+
+    public MidiChannel Channel;
+
+	public virtual void OnClickEnter(Vector3 inWorldMousePos)
+    {
+        LightMatInstance.color = ClickedColor;
+    }
+
+    public virtual void OnClickExit(Vector3 inWorldMousePos)
+    {
+        LightMatInstance.color = BaseColor;
+    }
+
+    public virtual void OnClickStay(Vector3 inWorldMousePos)
+    {
+        
+    }
+
+
+    protected virtual void Start()
+    {
+        LightMatInstance = GetInstanceMaterial(ButtonLightMat);
+        LightMatInstance.color = BaseColor;
+
+    }
+
+    protected Material GetInstanceMaterial(Material in_Material)
+    {
+
+         
+        foreach(InteractableItem item in gameObject.GetComponents<InteractableItem>())
+        {
+            if (item.LightMatInstance != null)
+                return item.LightMatInstance;
+        }
+
+
+        Renderer[] AllRenderers = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer rend in AllRenderers)
+        {
+            for (int i = 0; i < rend.sharedMaterials.Length; i++)
+            {
+                if (rend.sharedMaterials[i].Equals(in_Material))
+                {
+                    return rend.materials[i];
+                }
+            }
+
+        }
+        return null;
+    }
+
+
+    protected Vector3 ProjectPointOnPlane(Vector3 in_Point)
+    {
+        Vector3 planeNormal = transform.up.normalized;
+        var distance = -Vector3.Dot(planeNormal.normalized, (in_Point - transform.position));
+        return in_Point + planeNormal * distance;
+    }
+
+}
---
+++ GameJam2018/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera PlayerCamera;
+
+
+    private InteractableItem currentClickedItem;
+    public float lastDepthDistance;
+
+    void Awake()
+    {
+        if (PlayerCamera == null)
+            PlayerCamera = GetComponentInChildren<Camera>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            InteractableItem hit = RayCastFromCamera();
+            if (hit != null)
+            {
+                hit.OnClickEnter(GetMouseUIPosition());
+                currentClickedItem = hit;
+            }
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            if (currentClickedItem != null)
+            {
+                currentClickedItem.OnClickExit(GetMouseUIPosition());
+                currentClickedItem = null;
+            }
+        }
+        else if (currentClickedItem != null)
+            currentClickedItem.OnClickStay(GetMouseUIPosition());
+
+
+    }
+
+    private InteractableItem RayCastFromCamera()
+    {
+
+        RaycastHit hit;
+        Ray ray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            InteractableItem hitItem = hitObject.GetComponentInParent<InteractableItem>();
+            if (hitItem != null)
+            {
+                lastDepthDistance = Vector3.Distance(transform.position, hitItem.transform.position);
+                return hitItem;
+            }
+
+        }
+        return null;
+    }
+
+    private Vector3 GetMouseUIPosition()
+    {
+        RaycastHit hit;
+        Ray ray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit,100f, LayerMask.GetMask("PhysicalUI")))
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
+    }
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = lastDepthDistance;
+
+        return PlayerCamera.ScreenToWorldPoint(mousePos);
+    }
+
+    
+
+
+}
